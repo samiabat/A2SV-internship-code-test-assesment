0e4a8e7a9ec2d1adf0a409cbdf25e6cb09f20b11
samiabat <samuelabatneh20@gmail.com>
2023-03-27 20:30:54 +0300

    add comments

--- string_functions.py
+++ string_functions.py
@@ -1,5 +1,7 @@
 def to_upper(sentence):
     result = []
+    # if the character is small it ord value is between 97-122 inclusive
+    # we substract 32 from the ord value to change to a small letter.
     for char in sentence:
         if 97 <= ord(char) <= 122:
             result.append(chr(ord(char) - 32))
@@ -9,6 +11,9 @@ def to_upper(sentence):
 
 def to_lower(sentence):
     result = []
+    # if character is capital there ord value is between 65-90 inclusive
+    # we will add 32 to their ord value to change to capital leter
+    # we will collect those and return by joining them
     for char in sentence:
         if 65 <= ord(char) <= 90:
             result.append(chr(ord(char) + 32))
@@ -18,7 +23,9 @@ def to_lower(sentence):
 
 def capitalize(sentence):
     result = ""
-
+    # if there is no character in sentence we will return empty 
+    # other wise we will change the first characters to capital letter and change the 
+    # remaining to small letter.
     if sentence:
         result += to_upper(sentence[0])
     result += to_lower(sentence[1:])
--- test.py
+++ test.py
@@ -5,43 +5,41 @@ class TestToUpper(unittest.TestCase):
     def test_to_upper(self):
         #Check the number, there couldn't be any change
         self.assertEqual(to_upper('43'), '43')
-        #Check the to_upper method with
         #different edge cases
-        self.assertEqual(to_upper('sefineh'), "SEFINEH")
-        self.assertEqual(to_upper('SEFINEH'), 'SEFINEH')
+        self.assertEqual(to_upper('hello world'), "HELLO WORLD")
+        self.assertEqual(to_upper('HELLO WORLD'), 'HELLO WORLD')
+        self.assertEqual(to_upper(''), '')
         #Check invalid conditions
         self.assertNotEqual(to_upper('False'), 'False')
         self.assertNotEqual(to_upper('False'), 'false')
-        self.assertNotEqual(to_upper('False'), 'FALSe')
-        self.assertNotEqual(to_upper('false'), 'fALSE')
-        self.assertNotEqual(to_upper('false'), 'False')
-        self.assertEqual(to_upper(''), '')
+        
+
 class TestToLower(unittest.TestCase):
     def test_to_lower(self):
         #Check the numbers as they couldn't be changed
         self.assertEqual(to_lower('54'), '54')
         self.assertEqual(to_lower('78'), '78')
         #Check the to_lower method with different edge cases 
-        self.assertEqual(to_lower('False'), 'false')
+        self.assertEqual(to_lower('Hello World'), 'hello world')
+        self.assertEqual(to_lower('HELLO WORLD'), 'hello world')
+        self.assertEqual(to_lower(''), '')
         #Check invalid conditions
         self.assertNotEqual(to_lower('False'), 'FALSe')
         self.assertNotEqual(to_lower('false'), 'fALSE')
-        self.assertNotEqual(to_lower('false'), 'False')
-        self.assertEqual(to_lower(''), '')
+        
         
 class TestCapitalize(unittest.TestCase):
     def test_capitalize(self):
         #Check with numbers as they couldn't be changed
         self.assertEqual(capitalize('90'), '90')
         #Check the capitalize method with different edge cases
-        self.assertEqual(capitalize('Sefineh'), 'Sefineh')
-        self.assertEqual(capitalize('sefineh'), 'Sefineh')
-        self.assertEqual(capitalize('SEFINEH'), 'Sefineh')
+        self.assertEqual(capitalize('Hello world'), 'Hello world')
+        self.assertEqual(capitalize('hello world'), 'Hello world')
+        self.assertEqual(capitalize('HELLO WORLD'), 'Hello world')
+        self.assertEqual(capitalize(''), '')
         #Check invalid conditions
-        self.assertNotEqual(capitalize('SEFINEH'), 'sEfINEH')
-        self.assertNotEqual(capitalize('Sefineh'), 'sefineh')
-        self.assertNotEqual(capitalize('sefineh'), 'sEfineh')
-        self.assertNotEqual(capitalize('SEFINEH'), 'SefineH')
+        self.assertNotEqual(capitalize('Hello World'), 'Hello WORld')
+        self.assertNotEqual(capitalize('Hello world'), 'hello world')
         
 
 if __name__ == '__main__':

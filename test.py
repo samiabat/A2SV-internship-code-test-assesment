2407957d9419c40b733384e2278ec4e55d48da93
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-03-27 19:26:21 +0300

    to_lower method is tested well and it is working

--- test.py
+++ test.py
@@ -6,24 +6,24 @@ class TestToUpper(unittest.TestCase):
         #Check the number, there couldn't be any change
         self.assertEqual(to_upper('43'), '43')
         self.assertEqual(to_lower('54'), '54')
-        #Check the to_upper and to_lower methods with
+        #Check the to_upper method with
         #different edge cases
         self.assertNotEqual(to_upper('False'), 'False')
         self.assertNotEqual(to_upper('False'), 'false')
         self.assertNotEqual(to_upper('False'), 'FALSe')
         self.assertNotEqual(to_upper('false'), 'fALSE')
         self.assertNotEqual(to_upper('false'), 'False')
+        self.assertEqual(to_upper(''), '')
+class TestToLower(unittest.TestCase):
+    def test_to_lower(self):
+        #Check the numbers as they couldn't be changed
+        self.assertEqual(to_lower('78'), '78')
+        #Check the to_lower method with different edge cases 
         self.assertEqual(to_lower('False'), 'false')
         self.assertNotEqual(to_lower('False'), 'FALSe')
         self.assertNotEqual(to_lower('false'), 'fALSE')
         self.assertNotEqual(to_lower('false'), 'False')
-class TestToLower(unittest.TestCase):
-    def test_to_lower(self):
-        
-        self.assertEqual(to_lower("WORLD"), "world")
-        self.assertEqual(to_lower("world"), "world")
-        self.assertEqual(to_lower("World"), "world")
-        self.assertEqual(to_lower(""), "")
+        self.assertEqual(to_lower(''), '')
         
 class TestCapitalize(unittest.TestCase):
     def test_capitalize(self):

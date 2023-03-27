8aff326d6200bcabea94682c6ec2136753b0ecd4
samiabat <samuelabatneh20@gmail.com>
2023-03-27 14:05:09 +0300

    add unit tests tests

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 import unittest
-from string_functions import *
+from string_functions import to_upper, to_lower, capitalize
 
 class TestToUpper(unittest.TestCase):
-    # write your code here
-    pass
-
+    def test_to_upper(self):
+        self.assertEqual(to_upper("hello"), "HELLO")
+        self.assertEqual(to_upper("HELLO"), "HELLO")
+        self.assertEqual(to_upper("Hello"), "HELLO")
+        self.assertEqual(to_upper(""), "")
+        
 class TestToLower(unittest.TestCase):
-    # Write you code here
-    pass
-
+    def test_to_lower(self):
+        self.assertEqual(to_lower("WORLD"), "world")
+        self.assertEqual(to_lower("world"), "world")
+        self.assertEqual(to_lower("World"), "world")
+        self.assertEqual(to_lower(""), "")
+        
 class TestCapitalize(unittest.TestCase):
-    # Write your code here
-    pass
-
+    def test_capitalize(self):
+        self.assertEqual(capitalize("hello world"), "Hello world")
+        self.assertEqual(capitalize("HELLO WORLD"), "Hello world")
+        self.assertEqual(capitalize("Hello World"), "Hello world")
+        self.assertEqual(capitalize(""), "")
+        
 if __name__ == '__main__':
     unittest.main()
